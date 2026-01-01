29f4ade6564c7449b24a460b45ceda13c2cdd15a
Gayatri <140253982+Gayatrieva@users.noreply.github.com>
2026-01-01 14:16:31 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 export const dbConnect = async () => {
-  const conn = await mongoose.connect('mongodb://localhost:27017/NewsPortal')
+  const conn = await mongoose.connect('mongodb+srv://db_user:T8tzTrYE7FoImXYF@freenews.uwmgavl.mongodb.net/?appName=freenews')
   if (conn) {
 
     console.log("DB Connected Successfully.........");
